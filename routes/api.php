afa7244b99b7f988b3c292c0a52dcdf467645598
bforbilly24 <halimputra0701@gmail.com>
2024-03-24 00:19:34 +0700

    refactor(routes): update route function for api xml-data

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/xml-data', [XmlController::class, 'getXmlData']);
+Route::get('/xml-data', [XmlController::class, 'parse']);
