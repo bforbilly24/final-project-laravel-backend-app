54166b9e14a3dc88d38e7b972384873f677c66ff
bforbilly24 <halimputra0701@gmail.com>
2024-04-03 22:45:41 +0700

    refacator(Services): remove function unused

--- app/Services/XmlService.php
+++ app/Services/XmlService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Storage;
-use SimpleXMLElement;
 
 class XmlService
 {
@@ -18,20 +17,5 @@ class XmlService
         } catch (\Exception $e) {
             return null;
         }
-    }
-
-    private function xmlToArray(SimpleXMLElement $xml)
-    {
-        $array = (array)$xml;
-
-        foreach (array_slice($array, 0) as $key => $value) {
-            if (strlen($key) > 20) {
-                unset($array[$key]);
-            } else if (is_object($value) && get_class($value) == 'SimpleXMLElement') {
-                $array[$key] = $this->xmlToArray($value);
-            }
-        }
-
-        return $array;
     }
 }
