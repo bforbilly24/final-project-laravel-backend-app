2d403073b0aa735bffb2ce53054520130c21f3b4
bforbilly24 <halimputra0701@gmail.com>
2024-04-03 22:44:30 +0700

    refactor(Controllers): update function store for save xml file into storage/xml-data, add function for parse xml to json by latest

--- app/Http/Controllers/Api/RkksController.php
+++ app/Http/Controllers/Api/RkksController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Rkks;
 use App\Services\XmlService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RkksController extends Controller
 {
@@ -33,29 +34,53 @@ class RkksController extends Controller
 
         // Parse the XML file
         // $array = $xmlService->parseXml($rkksXml->getRealPath());
+        $path = $rkksXml->store('xml-data');
 
         // Store the name of the XML file
         Rkks::create([
-            'xmlFileName' => $rkksXml->getClientOriginalName(),
+            'xmlFileName' => $path,
         ]);
+
+        // Check if the number of records is more than 10
+        if (Rkks::count() > 10) {
+            // Get the oldest record
+            $oldestRecord = Rkks::oldest()->first();
+
+            // Delete the XML file
+            Storage::delete($oldestRecord->xmlFileName);
+
+            // Delete the record from the database
+            $oldestRecord->delete();
+        }
 
         return response()->json([
             'status' => 'success',
             'data' => 'Rkks created successfully',
         ]);
     }
+    protected $xmlService;
 
-    // protected $xmlService;
+    public function __construct(XmlService $xmlService)
+    {
+        $this->xmlService = $xmlService;
+    }
 
-    // public function __construct(XmlService $xmlService)
-    // {
-    //     $this->xmlService = $xmlService;
-    // }
+    public function parse()
+    {
+        // Get the latest XML file name from the database
+        $latestXmlFile = Rkks::latest()->first();
 
-    // public function parse()
-    // {
-    //     $filePath = 'xml/d_trktrm02318006776042.xml';
-    //     $array = $this->xmlService->parseXml($filePath);
-    //     return response()->json($array);
-    // }
+        // If there's no record in the database, return an error message
+        if (!$latestXmlFile) {
+            return response()->json(['error' => 'No XML file found'], 404);
+        }
+
+        // Construct the file path
+        $filePath = $latestXmlFile->xmlFileName;
+
+        // Parse the XML file to JSON
+        $array = $this->xmlService->parseXml($filePath);
+
+        return response()->json($array);
+    }
 }
