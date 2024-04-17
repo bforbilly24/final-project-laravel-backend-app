ee2f34bf137ce86eb40a2e6f92667089b2062509
bforbilly24 <halimputra0701@gmail.com>
2024-04-18 00:48:16 +0700

    refactor(Controllers): comment username

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only(
-            'username',
+            // 'username',
             'email',
             'password'
         );
