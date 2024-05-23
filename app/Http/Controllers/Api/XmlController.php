f633ab1c471dd3043bed31d06ba77c2c491b7743
bforbilly24 <halimputra0701@gmail.com>
2024-05-23 18:21:23 +0700

    feat(Controllers): add handler error if  XML file contains a column that is not in the alias list

--- app/Http/Controllers/Api/XmlController.php
+++ app/Http/Controllers/Api/XmlController.php
@@ -27,6 +27,10 @@ class XmlController extends Controller
         // Parse and map the XML content to database fields
         $data = $this->mapXmlToDatabaseFields($xmlContent);
 
+        if (isset($data['error'])) {
+            return response()->json(['error' => $data['error']], 400);
+        }
+
         // Insert the data into the database
         DB::table('xml_data')->insert($data);
 
@@ -38,48 +42,61 @@ class XmlController extends Controller
         // Load the XML content
         $xml = simplexml_load_string($xmlContent);
         $data = [];
+        $requiredFields = [
+            'thang', 'kdjendok', 'kdsatker', 'kddept', 'kdunit', 'kdprogram',
+            'kdgiat', 'kdoutput', 'kdlokasi', 'kdkabkota', 'kddekon', 'kdsoutput',
+            'kdkmpnen', 'kdskmpnen', 'kdakun', 'kdkppn', 'kdbeban', 'kdjnsban',
+            'kdctarik', 'register', 'carahitung', 'prosenphln', 'prosenrkp', 'prosenrmp',
+            'kppnrkp', 'kppnrmp', 'kppnphln', 'regdam', 'kdluncuran', 'kdib',
+            'levelrev', 'revdipake', 'uraiblokir', 'kdblokir'
+        ];
 
         // Loop through all child elements of the root
         foreach ($xml->children() as $element) {
-            // Check if the element has the expected child structure
-            if (isset($element->thang)) {
-                $data[] = [
-                    'A'  => (string) $element->thang,
-                    'B'  => (string) $element->kdjendok,
-                    'C'  => (string) $element->kdsatker,
-                    'D'  => (string) $element->kddept,
-                    'E'  => (string) $element->kdunit,
-                    'F'  => (string) $element->kdprogram,
-                    'G'  => (string) $element->kdgiat,
-                    'H'  => (string) $element->kdoutput,
-                    'I'  => (string) $element->kdlokasi,
-                    'J'  => (string) $element->kdkabkota,
-                    'K'  => (string) $element->kddekon,
-                    'L'  => (string) $element->kdsoutput,
-                    'M'  => (string) $element->kdkmpnen,
-                    'N'  => (string) $element->kdskmpnen,
-                    'O'  => (string) $element->kdakun,
-                    'P'  => (string) $element->kdkppn,
-                    'Q'  => (string) $element->kdbeban,
-                    'R'  => (string) $element->kdjnsban,
-                    'S'  => (string) $element->kdctarik,
-                    'T'  => (string) $element->register,
-                    'U'  => (string) $element->carahitung,
-                    'V'  => (string) $element->prosenphln,
-                    'W'  => (string) $element->prosenrkp,
-                    'X'  => (string) $element->prosenrmp,
-                    'Y'  => (string) $element->kppnrkp,
-                    'Z'  => (string) $element->kppnrmp,
-                    'AA' => (string) $element->kppnphln,
-                    'BB' => (string) $element->regdam,
-                    'CC' => (string) $element->kdluncuran,
-                    'DD' => (string) $element->kdib,
-                    'FF' => (string) $element->levelrev,
-                    'GG' => (string) $element->revdipake,
-                    'HH' => (string) $element->uraiblokir,
-                    'II' => (string) $element->kdblokir,
-                ];
+            // Check if the element has all the required fields
+            foreach ($requiredFields as $field) {
+                if (!isset($element->$field)) {
+                    return ['error' => 'The XML file cannot be uploaded because the format is not suitable'];
+                }
             }
+
+            // If all required fields are present, map them to the database fields
+            $data[] = [
+                'A'  => (string) $element->thang,
+                'B'  => (string) $element->kdjendok,
+                'C'  => (string) $element->kdsatker,
+                'D'  => (string) $element->kddept,
+                'E'  => (string) $element->kdunit,
+                'F'  => (string) $element->kdprogram,
+                'G'  => (string) $element->kdgiat,
+                'H'  => (string) $element->kdoutput,
+                'I'  => (string) $element->kdlokasi,
+                'J'  => (string) $element->kdkabkota,
+                'K'  => (string) $element->kddekon,
+                'L'  => (string) $element->kdsoutput,
+                'M'  => (string) $element->kdkmpnen,
+                'N'  => (string) $element->kdskmpnen,
+                'O'  => (string) $element->kdakun,
+                'P'  => (string) $element->kdkppn,
+                'Q'  => (string) $element->kdbeban,
+                'R'  => (string) $element->kdjnsban,
+                'S'  => (string) $element->kdctarik,
+                'T'  => (string) $element->register,
+                'U'  => (string) $element->carahitung,
+                'V'  => (string) $element->prosenphln,
+                'W'  => (string) $element->prosenrkp,
+                'X'  => (string) $element->prosenrmp,
+                'Y'  => (string) $element->kppnrkp,
+                'Z'  => (string) $element->kppnrmp,
+                'AA' => (string) $element->kppnphln,
+                'BB' => (string) $element->regdam,
+                'CC' => (string) $element->kdluncuran,
+                'DD' => (string) $element->kdib,
+                'FF' => (string) $element->levelrev,
+                'GG' => (string) $element->revdipake,
+                'HH' => (string) $element->uraiblokir,
+                'II' => (string) $element->kdblokir,
+            ];
         }
 
         return $data;
