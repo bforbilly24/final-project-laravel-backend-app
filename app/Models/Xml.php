008d67c219f795eec56ce55910d904a6b9d79843
bforbilly24 <halimputra0701@gmail.com>
2024-05-23 17:38:55 +0700

    feat(Models): add /Models/Xml

---
+++ app/Models/Xml.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Xml extends Model
+{
+    use HasFactory;
+
+    protected $table = 'xml_data';
+
+    protected $fillable = [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        'AA',
+        'BB',
+        'CC',
+        'DD',
+        'FF',
+        'GG',
+        'HH',
+        'II',
+    ];
+}
