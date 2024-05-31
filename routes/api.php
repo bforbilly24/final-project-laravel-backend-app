036a52d5cf247348d4ea3398ba6a24db162d3023
bforbilly24 <halimputra0701@gmail.com>
2024-05-31 14:48:14 +0700

    refactor(route): update route for api

--- routes/api.php
+++ routes/api.php
@@ -20,17 +20,21 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-// read xml to json
-Route::get('/xml-data', [RkksController::class, 'parse']);
-// create for uploaded xml file
-Route::post('/rkks-create', [RkksController::class, 'store']);
-// read xml data from database
-Route::get('/rkks', [RkksController::class, 'index']);
 
-// loogin
+// login
 Route::post('/login', [AuthController::class, 'login']);
 
-
-// XML upload routes
-Route::post('/upload-xml', [XmlController::class, 'upload']);
+// XML SAVE AS ALIASED COLOUMN CHILDREN
+// upload for xml file
+Route::post('/upload-xml', [XmlController::class, 'uploadXml']);
+// read xml data from database
 Route::get('/xml-data', [XmlController::class, 'getData']);
+
+
+// XML SAVE AS PATH
+// read xml to json
+// Route::get('/xml', [RkksController::class, 'parse']);
+// create for xml file
+// Route::post('/rkks-create', [RkksController::class, 'store']);
+// read xml data from database
+// Route::get('/rkks', [RkksController::class, 'index']);
